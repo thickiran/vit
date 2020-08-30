6facddb24105ec70b17c520b5ef4dd729cfd05c7
Tolgay Hickiran <tolgayhickiran@gmail.com>
2020-08-31 00:33:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ wwwroot/js/colours.js
@@ -0,0 +1,19 @@
+﻿function black() {
+    Object.values(NOP_VIEWER.impl.matman()._materials)[Object.values(NOP_VIEWER.impl.matman()._materials).length - 2].color = new THREE.Color(0.2, 0.2, 0.2);
+    NOP_VIEWER.impl.invalidate(true, true, false);
+}
+
+function white() {
+    Object.values(NOP_VIEWER.impl.matman()._materials)[Object.values(NOP_VIEWER.impl.matman()._materials).length - 2].color = new THREE.Color(0.98, 0.98, 0.98);
+    NOP_VIEWER.impl.invalidate(true, true, false);
+}
+
+function yellow() {
+    Object.values(NOP_VIEWER.impl.matman()._materials)[Object.values(NOP_VIEWER.impl.matman()._materials).length - 2].color = new THREE.Color(0.98, 0.98, 0);
+    NOP_VIEWER.impl.invalidate(true, true, false);
+}
+
+function red() {
+    Object.values(NOP_VIEWER.impl.matman()._materials)[Object.values(NOP_VIEWER.impl.matman()._materials).length - 2].color = new THREE.Color(0.98, 0, 0);
+    NOP_VIEWER.impl.invalidate(true, true, false);
+}
